9bde1c7385b9a666724803af0f56bcd8d18a0dd2
zawa1120 <ckz1120@gmail.com>
2021-12-08 20:40:36 +0900

    main

---
+++ 二分探索.py
@@ -0,0 +1,12 @@
+def binary_search(data, value):
+    left = 0
+    right = len(data) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if data[mid] == value:
+            return mid
+        elif data[mid] < value:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1 
