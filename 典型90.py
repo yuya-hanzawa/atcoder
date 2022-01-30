cf6d2333d2d58fdee54e94b2f6878236200678f7
zawa1120 <ckz1120@gmail.com>
2022-01-31 00:40:06 +0900

    main

---
+++ 典型90.py
@@ -0,0 +1,8 @@
+# Level 2
+# 67: https://atcoder.jp/contests/typical90/tasks/typical90_bo
+
+# Level 3
+# 02: https://atcoder.jp/contests/typical90/tasks/typical90_b
+# 18: https://atcoder.jp/contests/typical90/tasks/typical90_r
+# 
+ 
